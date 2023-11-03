29652be40c08f3471d58693663ece8984b3ea3b1
Chorn Thorn <chornthorn.kh@gmail.com>
2023-11-03 09:32:01 +0700

    First Init

---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+    return {
+      data: {
+        ...createUserDto,
+      },
+    };
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return {
+      data: {
+        ...updateUserDto,
+      },
+    };
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
